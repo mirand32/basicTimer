63875b581012a2be185fa16462b55cbcc12f48f6
Stefan <stefan.f.miranda@gmail.com>
2020-12-14 20:11:39 -0500

    refactor code

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
+// Get DOM Elements
 const durationInput=document.querySelector("#duration")
 const startButton=document.querySelector("#start")
 const pauseButton=document.querySelector("#pause")
 const circle=document.querySelector("circle")
+// Create vars for offset and perimeter to update animation
 let offset
 let perimeter
-
+// Use timer class to construct new timer
 const timer=new Timer(durationInput,startButton,pauseButton,{
     onStart(duration){
+        // get perimeter of circle
         perimeter = parseFloat(circle.getAttribute("r")) * 2 * Math.PI
+        // update timeInit to current duration in input
         this.timeInit=duration
+        // set offset to zero
         offset=0
+        // initialize circle svg stroke to cover full perimeter of circle
         circle.setAttribute("stroke-dasharray", perimeter)
         circle.setAttribute("stroke-dashoffset", 0)
 
     },
     onTick(){
+        // update circle's stroke-offset on on each tick() to animate countdown
         circle.setAttribute("stroke-dashoffset", offset)
-        const increment= -(perimeter/this.timeInit)*0.02
-        offset += increment
-    },
-    onComplete(){
-    } 
+        // calc amount dashoffset should update each tick 
+        const incrementCircle= -(perimeter/this.timeInit)*this.increment
+        offset += incrementCircle
+    }
 })
--- timer.js
+++ timer.js
@@ -1,49 +1,61 @@
 class Timer {
     constructor(durationInput, startButton, pauseButton, callbacks) {
+        // get DOM Elements
         this.durationInput = durationInput
         this.startButton = startButton
         this.pauseButton = pauseButton
         this.timeInit=durationInput.value
+        this.increment=0.02
+        // Check for callbacks and add to Timer Object if given
         if (callbacks) {
             console.log(true)
             this.onStart = callbacks.onStart
             this.onTick = callbacks.onTick
             this.onComplete = callbacks.onComplete
         }
+        // Event listeners 
         this.durationInput.addEventListener("change", this.onChangeTimer)
         this.startButton.addEventListener("click", this.start)
         this.pauseButton.addEventListener("click", this.pause)
     }
     start = () => {
+        // run onStart callback if given and pass in current time remaining
         if (this.onStart) {
             this.onStart(this.timeRemaining)
         }
-        this.tick()
-        this.interval = setInterval(this.tick, 20)
+        // run tick function every x milliseconds
+        this.interval = setInterval(this.tick, this.increment*1000)
     }
+    // clear interval to stop running tick
     pause = () => {
         clearInterval(this.interval)
     }
-
+    // on change of time input change initTime
     onChangeTimer = (e) => {
-        this.timeInit=parseFloat(e.target.innerHTML).toFixed
+        this.timeInit=parseFloat(e.target.innerHTML).toFixed(2)
     }
     tick = () => {
+        // check if timer reached zero
         if (this.timeRemaining <= 0) {
+            // if onComplete callback is given call it
             if (this.onComplete) {
                 this.onComplete()
             }
+            // stop timer
             this.pause()
         } else {
-            this.timeRemaining = this.timeRemaining - 0.02
+            //adjust time remaining and run onTick callback if given
+            this.timeRemaining = this.timeRemaining - this.increment
             if (this.onTick) {
                 this.onTick()
             }
         }
     }
+    // getter for time remaining in duration input
     get timeRemaining() {
         return parseFloat(this.durationInput.value)
     }
+    // setter for time remaining in duration input
     set timeRemaining(time) {
         this.durationInput.value = time.toFixed(2)
     }
